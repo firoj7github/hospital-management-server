159af6e4868606ea95cf622bc515930d3aed22c6
firoj <fkfiroj02@gmail.com>
2022-12-22 22:47:55 -0800

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.port || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -13,6 +14,12 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusterhospital.66shuij.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+  const authHeader= req.headers.authorization;
+  if(!authHeader){
+    return res.status('401').send({message:'unauthorized access'});
+  }
+};
 
 async function run(){
   try{
@@ -20,6 +27,19 @@ async function run(){
        console.log('database connected');
        const serviceCollection=client.db('hospital_management').collection('services');
        const bookingCollection=client.db('hospital_management').collection('bookings');
+       const userCollection=client.db('hospital_management').collection('users');
+
+       app.put('/user/:email', async(req,res)=>{
+        const email= req.params.email;
+        const user = req.body;
+        const filter={email: email};
+        const options ={upsert: true};
+        const updateDoc={$set: user};
+        const result= await userCollection.updateOne(filter, updateDoc, options);
+        const token= jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRATE, { expiresIn: '1h' } )
+        res.send({result, token}); 
+
+       })
 
        app.get('/service', async(req,res)=>{
         const query={};
@@ -27,6 +47,20 @@ async function run(){
         const services = await cursor.toArray();
         res.send(services);
        })
+
+      //  get fixed email appionment
+
+      app.get('/booking',verifyJWT, async(req,res)=>{
+        // const patientEmail=req.query.patientEmail;
+        // const query= {patientEmail: patientEmail};
+        // const authorization= req.headers.authorization;
+        console.log(authorization);
+        const query={}
+        const cursor = bookingCollection.find(query);
+        const bookings = await cursor.toArray();
+        res.send(bookings);
+       
+      })
 
     
      //  post
@@ -38,7 +72,7 @@ async function run(){
         return res.send({success:false})
       }
       const result = await bookingCollection.insertOne(booking);
-      res.send(result);
+      res.send({success:true, result});
   
     })
   }
